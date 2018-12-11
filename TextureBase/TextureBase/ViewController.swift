0106d0f81d3f8c2cb40a2bd74c6b8034373ede17
HJQ <v-huangjiaqian@midland.local>
2018-12-11 10:02:12 +0800

    初始化工程

---
+++ TextureBase/TextureBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextureBase
+//
+//  Created by midland on 2018/12/11.
+//  Copyright © 2018年 ml. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
