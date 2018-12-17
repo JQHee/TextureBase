fa533dd10d4cf2e1b08658e53fdee41afaf240f0
HJQ <v-huangjiaqian@midland.local>
2018-12-17 10:02:23 +0800

    增加游戏资讯页面

---
+++ TextureBase/TextureBase/Classes/Sections/Select/GameNews/SelectGameNewsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SelectGameNewsViewController.swift
+//  TextureBase
+//
+//  Created by midland on 2018/12/17.
+//  Copyright © 2018年 ml. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+// MARK: - 游戏资讯
+class SelectGameNewsViewController: ASViewController<ASDisplayNode> {
+
+    // MARK: - Life cycle
+    init() {
+        super.init(node: ASDisplayNode())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
