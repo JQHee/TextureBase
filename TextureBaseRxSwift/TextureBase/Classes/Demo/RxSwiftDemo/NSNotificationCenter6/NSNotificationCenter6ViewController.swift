b6d2aff38cb5393f142d55250641dee58ebcff9d
huangjiaqian <18775134221@163.com>
2018-12-30 22:59:15 +0800

    提交

---
+++ TextureBaseRxSwift/TextureBase/Classes/Demo/RxSwiftDemo/NSNotificationCenter6/NSNotificationCenter6ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NSNotificationCenter6ViewController.swift
+//  TextureBase
+//
+//  Created by HJQ on 2018/12/30.
+//  Copyright © 2018 ml. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class NSNotificationCenter6ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NotificationCenter.default.rx.notification(Notification.Name.init(rawValue: "key"))
+            .takeUntil(self.rx.deallocated) //页面销毁自动移除通知监听
+            .subscribe(onNext: { (notification) in
+            print(notification.object ?? "")
+        })
+
+    }
+
+}
