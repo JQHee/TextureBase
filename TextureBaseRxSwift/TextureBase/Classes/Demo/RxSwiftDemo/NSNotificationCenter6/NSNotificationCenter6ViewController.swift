eec245cf30472e4df71bc2c0e47019fe4458b69d
huangjiaqian <18775134221@163.com>
2018-12-30 23:06:30 +0800

    提交

--- TextureBaseRxSwift/TextureBase/Classes/Demo/RxSwiftDemo/NSNotificationCenter6/NSNotificationCenter6ViewController.swift
+++ TextureBaseRxSwift/TextureBase/Classes/Demo/RxSwiftDemo/NSNotificationCenter6/NSNotificationCenter6ViewController.swift
@@ -15,11 +15,28 @@ class NSNotificationCenter6ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        NotificationCenter.default.rx.notification(Notification.Name.init(rawValue: "key"))
+        _ = NotificationCenter.default.rx.notification(Notification.Name.init(rawValue: "key"))
             .takeUntil(self.rx.deallocated) //页面销毁自动移除通知监听
             .subscribe(onNext: { (notification) in
-            print(notification.object ?? "")
+                print(notification.object ?? notification.userInfo ?? "")
         })
+
+
+        //监听键盘弹出通知
+        _ = NotificationCenter.default.rx
+            .notification(UIResponder.keyboardWillShowNotification)
+            .takeUntil(self.rx.deallocated) //页面销毁自动移除通知监听
+            .subscribe(onNext: { _ in
+                print("键盘出现了")
+            })
+
+        //监听键盘隐藏通知
+        _ = NotificationCenter.default.rx
+            .notification(UIResponder.keyboardWillHideNotification)
+            .takeUntil(self.rx.deallocated) //页面销毁自动移除通知监听
+            .subscribe(onNext: { _ in
+                print("键盘消失了")
+            })
 
     }
 
